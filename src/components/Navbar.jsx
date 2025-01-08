51be060af6e88ba2aa8eaa4502a3273768b2ef19
Priyangshu <shomepriyangshu16@gmail.com>
2025-01-08 12:38:21 +0530

    fixed the quick navigation for small devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,23 +26,24 @@ const Navbar = ({ dark, setDark }) => {
 
       {/* Navigation Links */}
       <div
-        className={`navLinks flex-col md:flex-row flex md:flex gap-6 md:gap-16 text-lg md:text-xl font-serif font-semibold absolute md:static top-16 left-0 w-full md:w-auto md:items-center transition-all duration-300 ease-in-out z-10 ${menuOpen
+        className={`navLinks flex-col md:flex-row flex md:flex gap-6 md:gap-16 text-lg md:text-xl font-serif font-semibold absolute md:static top-16 left-0 w-full md:w-auto md:items-center transition-all duration-300 ease-in-out z-20 ${
+          menuOpen
           ? dark
             ? "bg-gray-800 text-white border-2"
             : "bg-slate-100 text-black"
           : "hidden md:flex"
           }`}
       >
-        <a className="hover:text-orange-400 hover:underline px-6 py-2 md:p-0" href="/">
-          Home
+        <a className="hover:text-orange-400 hover:underline px-6 py-2 md:p-0" href="/" onClick={closeMenu}>
+          Home 
         </a>
-        <a className="hover:text-orange-400 hover:underline px-6 py-2 md:p-0" href="#about">
+        <a className="hover:text-orange-400 hover:underline px-6 py-2 md:p-0" href="#about" onClick={closeMenu}>
           About
         </a>
-        <a className="hover:text-orange-400 hover:underline px-6 py-2 md:p-0" href="#projects">
+        <a className="hover:text-orange-400 hover:underline px-6 py-2 md:p-0" href="#projects" onClick={closeMenu}>
           Projects
         </a>
-        <a className="hover:text-orange-400 hover:underline px-6 py-2 md:p-0" href="#contact">
+        <a className="hover:text-orange-400 hover:underline px-6 py-2 md:p-0" href="#contact" onClick={closeMenu}>
           Contact
         </a>
       </div>
